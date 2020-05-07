659aa10779971a89b0b2e674c232b520c2508e02
Irina Hristova <hristova119@gmail.com>
2020-05-07 19:06:36 +0300

    feat: init popularevents command

---
+++ 14.Tickets/Tickets/PopularEventsCommand.h
@@ -0,0 +1,9 @@
+#ifndef POPULAR_EVENTS_COMMAND_H
+#define POPULAR_EVENTS_COMMAND_H
+
+#include "ICommand.h"
+class PopularEventsCommand : public ICommand
+{
+};
+
+#endif // !POPULAR_EVENTS_COMMAND_H
