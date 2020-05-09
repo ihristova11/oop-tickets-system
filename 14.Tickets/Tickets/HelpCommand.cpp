7587a966dbe010682a73691726ca590627314f85
Irina Hristova <hristova119@gmail.com>
2020-05-09 09:29:16 +0300

    chore: fix typo in variable

--- 14.Tickets/Tickets/HelpCommand.cpp
+++ 14.Tickets/Tickets/HelpCommand.cpp
@@ -6,7 +6,7 @@ HelpCommand::HelpCommand(Receiver* receiver) : receiver(receiver)
 std::string HelpCommand::execute(const std::vector<std::string>&)
 {
 	// not interested in checking the parameters
-	return Constants::HelpRenspose;
+	return Constants::HelpRensponse;
 }
 
 std::string HelpCommand::toString()
