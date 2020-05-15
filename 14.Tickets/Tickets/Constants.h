f8f741bcb2f002ba1a815015dda5cc0cc5d2924d
Irina Hristova <hristova119@gmail.com>
2020-05-15 23:51:25 +0300

    feat: freeseats

--- 14.Tickets/Tickets/Constants.h
+++ 14.Tickets/Tickets/Constants.h
@@ -24,11 +24,11 @@ namespace Constants
 
 	const std::string NoHall = "No such hall exists!";
 
-	const std::string Success = "Success!";
+	const std::string Success = "Command completed successfully!";
 	const std::string Error = "Something's wrong with your command, try again!";
 
 	const std::string CloseSuccess = "Successfully closed file!"; // should have filename too
-	
+
 	const std::string HelpRensponse = "The following commands are supported:\n>open <file> - opens < file>\n>close - closes currently opened file\n>save - saves the currently open file\n>saveas <file> - saves the currently open file in <file>\n>help - prints this information\n>exit - exists the program\n>addevent <date> <hall> <name> - adds an event\n>freeseats <date> <name> - displays info for free seats\n>book <row> <seat> <date> <name> <note> - books a ticket\n>unbook <row> <seat> <date> <name> - unbooks a booked ticket\n>buy <row> <seat> <date> <name> - buys a ticket\n>bookings <date> <name> - displays info for sold and booked tickets\n>check <code> - checks if ticket is valid\n>report <from> <to> <hall> - displays info for events\npopularevents - displays info for the most watched events";
 	const std::string ExitResponse = "Exiting the program...";
 
@@ -36,8 +36,9 @@ namespace Constants
 	const std::string InvalidCommand = "Enter a valid command.";
 	const std::string InvalidFileName = "You can save only txt files.";
 	const std::string ErrorOpeningFile = "The file cannot be opened!";
+	const std::string NoEvent = "No such event found.";
 
-	const std::string AddEventError = "Sorry, this hall does not exist or is taken. Please choose another one!"; 
+	const std::string AddEventError = "Sorry, this hall does not exist or is taken. Please choose another one!";
 	const std::string SeatNotAvailable = "Sorry, this seat is taken or there is no such event! Choose another one.";
 	const std::string UnbookError = "There is nothing to unbook! Thank you!";
 	const std::string CheckFound = "This ticket is valid!";
--- 14.Tickets/Tickets/FreeSeatsCommand.cpp
+++ 14.Tickets/Tickets/FreeSeatsCommand.cpp
@@ -7,8 +7,41 @@ FreeSeatsCommand::FreeSeatsCommand(Store* store)
 
 std::string FreeSeatsCommand::execute(const std::vector<std::string>& parameters)
 {
+	// validate number of params
+	std::string date = parameters[1];
+	std::string name = parameters[2];
 
-	return Constants::Success;
+	Event* res = this->store->getEvent(date, name);
+	if (res != nullptr)
+	{
+		// get all places in hall
+		int all = res->hall.rows * res->hall.seatsPerRow;
+		bool* seats = new bool[all] { 0 };
+		int ind, row, seat;
+		// fill in with non-empty seats
+		for (int i = 0; i < res->tickets.size(); i++)
+		{
+			ind = res->tickets[i].seat + res->tickets[i].row * res->hall.seatsPerRow;
+			seats[ind] = 1;
+		}
+
+		std::cout << "Free seats:" << std::endl;
+		//print the empty seats
+		for (int i = 0; i < all; i++)
+		{
+			if (!seats[i])
+			{
+				row = i / res->hall.seatsPerRow;
+				seat = i - row * res->hall.seatsPerRow;
+				std::cout << "Row: " << row << " Seat: " << seat << std::endl;
+			}
+		}
+
+		delete[] seats;
+		return Constants::Success;
+	}
+
+	return Constants::NoEvent;
 }
 
 std::string FreeSeatsCommand::toString()
